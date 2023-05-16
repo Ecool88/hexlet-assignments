dd4966f2542f1e359805c1b2a1925a5296239200
Ecool88 <kulik0v.e.e@yandex.ru>
2023-05-16 11:49:41 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -9,35 +9,24 @@ import org.junit.jupiter.api.Test;
 class AppTest {
 
     @Test
-    void testTake1() {
+    void testTake() {
         // BEGIN
-        List<Integer> expected = List.of(1,2);
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
 
-        List<Integer> actual = App.take(numbers, 2);
-        assertThat(actual).isEqualTo(expected);
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> expected1 = new ArrayList<>();
+        List<Integer> result1 = App.take(list1, 3);
+        assertThat(result1).isEqualTo(expected1);
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> result2 = App.take(list2, 10);
+        assertThat(result2).isEqualTo(expected2);
+
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> expected3 = new ArrayList<>(Arrays.asList(1, 2));
+        List<Integer> result3 = App.take(list3, 2);
+        assertThat(result3).isEqualTo(expected3);
         // END
     }
 
-    @Test
-    void testTake2() {
-        // BEGIN
-        List<Integer> expected = List.of(7, 3, 10);
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(7, 3, 10));
-
-        List<Integer> actual = App.take(numbers, 8);
-        assertThat(actual).isEqualTo(expected);
-        // END
-    }
-
-    @Test
-    void testTake3() {
-        // BEGIN
-        List<Integer> expected = List.of();
-        List<Integer> numbers = new ArrayList<>(Arrays.asList());
-
-        List<Integer> actual = App.take(numbers, 1);
-        assertThat(actual).isEqualTo(expected);
-        // END
-    }
 }
